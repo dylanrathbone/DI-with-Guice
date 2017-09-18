0497b447f1720a0d9107d900f92357584cddc3c2
Dylan Rathbone <dylan.rathbone@pwc.com>
2017-09-18 14:16:17 +0100

    remove redundant files

--- README.md
+++
@@ -1,4 +0,0 @@
-DI-with-Guice
-==============
-
-This is a small example demonstrating Google Guice's use as a DI framework. 
--- src/main/java/com/dylanrathbone/interfacesfun/Datasource.java
+++
@@ -1,9 +0,0 @@
-package com.dylanrathbone.interfacesfun;
-
-public interface Datasource {
-
-    public String establishConnection();
-    public String terminateConnection();
-    public String getConnectionStatus();
-
-}
